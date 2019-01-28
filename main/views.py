4c4f5e9dc9a7b8f010b4407464d63d83016df4ec
Abhash Jha <abhashjha16@gmail.com>
2019-01-27 20:08:23 -0600

    Added login page for evaluator

--- main/views.py
+++ main/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'main/homepage.html', {})
 
+
+def evaluatorhome(request):
+    return render(request, 'main/evaluatorhome.html', {})
+
+
+
